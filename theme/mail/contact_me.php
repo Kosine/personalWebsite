cf96f464e97ebcb0c2e6750a5d72edb42bdd2472
Sanjay Lindsay <Sanjay Lindsay>
2016-07-26 23:56:07 -0400

    Add contact me form

---
+++ theme/mail/contact_me.php
@@ -0,0 +1,20 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $from = 'From: Sanjay Website';
+  $to = 'sanjayl@outlook.com';
+  $subject = 'Comment';
+  $human = $_POST['human'];
+
+  $body = "From: $name\n E-mail: $email\n Message: \n $message";
+  if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) {
+      echo '<p>Your message has been sent!</p>';
+    } else {
+      echo '<p>Something went wrong, go back and try again</p>';
+    }
+  } else if ($_POST['submit'] && $human != '4') {
+    echo '<p>You answered the anti-spam question incorrectly!</p>';
+  }
+ ?>
